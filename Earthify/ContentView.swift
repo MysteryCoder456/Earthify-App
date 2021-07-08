87ab307ae2143345123342ed62702be2f619dd54
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-07-08 19:52:15 +0400

    Initial Commit

---
+++ Earthify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Earthify
+//
+//  Created by Rehatbir Singh on 08/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Earthify/EarthifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthifyApp.swift
+//  Earthify
+//
+//  Created by Rehatbir Singh on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct EarthifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
