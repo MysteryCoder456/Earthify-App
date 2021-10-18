401b231f165af1554e59b86eb927211d657b4811
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-10-18 17:54:13 +0400

    settings menu

--- Earthify/TabViews/SettingsMenu.swift
+++ Earthify/TabViews/SettingsMenu.swift
@@ -42,6 +42,12 @@ struct SettingsMenu: View {
     @State var showingAlert = false
 
     @StateObject private var signInAlertCaller = NotificationHandler(notificationName: "UserSignedIn")
+    
+    let l_manageListings = NSLocalizedString("settingsmenu.manage_listings", comment: "Manage listings")
+    let l_about = NSLocalizedString("settingsmenu.about", comment: "About")
+    let l_signOut = NSLocalizedString("settingsmenu.sign_out", comment: "Sign Out")
+    let l_googleSignIn = NSLocalizedString("settingsmenu.google_sign_in", comment: "Sign in with Google")
+    let l_navTitle = NSLocalizedString("settingsmenu.nav_title", comment: "Settings")
 
     func signInWithGoogle() {
         env.googleAuthHandler.signInWithGoogle()
@@ -61,12 +67,12 @@ struct SettingsMenu: View {
             List {
                 if env.authenticated {
                     NavigationLink(destination: ManageListingsView()) {
-                        Text("Manage listings")
+                        Text(l_manageListings)
                     }
                 }
 
                 NavigationLink(destination: AboutView()) {
-                    Text("About")
+                    Text(l_about)
                 }
 
                 if env.authenticated {
@@ -74,19 +80,19 @@ struct SettingsMenu: View {
                         activeAlert = .signOut
                         showingAlert = true
                     }) {
-                        Text("Sign Out")
+                        Text(l_signOut)
                             .bold()
                             .foregroundColor(.red)
                     }
                 } else {
                     Button(action: signInWithGoogle) {
-                        Text("Sign in with Google")
+                        Text(l_googleSignIn)
                             .bold()
                             .foregroundColor(.blue)
                     }
                 }
             }
-            .navigationBarTitle("Settings", displayMode: .inline)
+            .navigationBarTitle(l_navTitle, displayMode: .inline)
         }
         .navigationViewStyle(.stack)
         .alert(isPresented: $showingAlert) {
@@ -95,17 +101,26 @@ struct SettingsMenu: View {
             switch activeAlert {
             case .signIn:
                 let currentUserEmail = (Auth.auth().currentUser?.email)!
+                
+                let l_title = NSLocalizedString("settingsmenu.sign_in_alert.success", comment: "Sign In Successful")
+                let l_msg = NSLocalizedString("settingsmenu.sign_in_alert.success_msg", comment: "You have been signed in as \(currentUserEmail).")
+                let l_dismissBtn = NSLocalizedString("settingsmenu.sign_in_alert.dismiss", comment: "OK")
+                
                 alert = Alert(
-                    title: Text("Sign In Successful"),
-                    message: Text("You have been signed in as \(currentUserEmail)."),
-                    dismissButton: .default(Text("OK"))
+                    title: Text(l_title),
+                    message: Text(l_msg),
+                    dismissButton: .default(Text(l_dismissBtn))
                 )
 
             case .signOut:
+                let l_title = NSLocalizedString("settingsmenu.sign_out_alert", comment: "Are you sure you want to sign out?")
+                let l_cancelBtn = NSLocalizedString("settingsmenu.sign_out_alert.cancel", comment: "Cancel")
+                let l_signOutBtn = NSLocalizedString("settingsmenu.sign_out_alert.sign_out", comment: "Sign Out")
+                
                 alert = Alert(
-                    title: Text("Are you sure you want to sign out?"),
-                    primaryButton: .default(Text("Cancel")),
-                    secondaryButton: .destructive(Text("Sign Out"), action: signOut)
+                    title: Text(l_title),
+                    primaryButton: .default(Text(l_cancelBtn)),
+                    secondaryButton: .destructive(Text(l_signOutBtn), action: signOut)
                 )
             }
 
